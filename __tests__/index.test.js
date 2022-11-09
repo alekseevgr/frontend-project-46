270ea53b40ba9104f124218718091f1238b440fd
alekseevgr <alekseevgr2@gmail.com>
2022-11-09 10:10:45 +0500

    add json test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,2 +1,27 @@
-import genDiff from "../src/index";
+import fs from 'fs';
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const tests = [
+  {
+    file1: 'file1.json', file2: 'file2.json', output: 'test1.json',
+  }
+];
+
+test.each(tests)('gendiff test', ({
+  file1, file2, output
+}) => {
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const expected = readFile(output);
+  const result = genDiff(filepath1, filepath2);
+  expect(result).toEqual(expected);
+});
